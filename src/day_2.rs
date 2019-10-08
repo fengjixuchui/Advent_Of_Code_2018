6b3f697bbac4785e1b4c418ebea37b5cac7209c0
mfrench <mfrench@indeed.com>
2019-10-08 18:34:04 -0500

    Day 1: Completed

---
+++ src/day_2.rs
@@ -0,0 +1,29 @@
+mod utility;
+//use utility::utility::*;
+use utility::stopwatch::stopwatch::*;
+//use std::collections::HashMap;
+
+fn part_2() {
+    //let input = load_file("resources/day_1_input.txt");
+
+    let stopwatch = Stopwatch::start_new();
+
+
+
+    stopwatch.print_elapsed();
+}
+
+fn part_1() {
+    //let input = load_file("resources/day_1_input.txt");
+
+    let stopwatch = Stopwatch::start_new();
+
+    stopwatch.print_elapsed();
+}
+
+fn main() {
+    println!("Part 1");
+    part_1();
+    println!("Part 2");
+    part_2();
+}
---
+++ src/utility/mod.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+pub mod stopwatch;
+pub mod utility {
+    use std::fs;
+
+    pub fn load_file(location : &str) -> String {
+        return match fs::read_to_string(location) {
+            Ok(string)  => string,
+            _ => {
+                eprint!("Failed to load {}", location);
+                String::from("")
+            }
+        };
+    }
+
+    pub fn split_by_new_line(input : String) -> Vec<String> {
+        input.lines().map(|string: &str| String::from(string)).collect::<Vec<String>>()
+    }
+
+    pub fn split_by_new_line_integer(input : String) -> Vec<i32> {
+        input.lines().map(|string: &str| {
+            match string.parse() {
+                Ok(number) => number,
+                _ => 0
+            }
+        }).collect::<Vec<i32>>()
+    }
+}
